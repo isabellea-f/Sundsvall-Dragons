0208ba8029f4f43f41cb2011005fb2d9decddd5d
Max Mattiasson <max@mattiasson.eu>
2025-09-15 09:54:23 +0200

    test weather

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const weatherCont = document.querySelector(".weather-cont");
+
 async function getWeather() {
   const res = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?lat=62.39129&lon=17.3063&appid=b2024be8c564ed764512c35c213e201c&lang=sv&units=metric"
   );
-  const data = res.json();
+  const data = await res.json();
+  const weatherDesc = data.weather[0].description;
+  const desc = document.createElement("p");
+  weatherDesc.append(desc);
+
+  const icon = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+  const img = document.createElement("img");
+  img.src = iconUrl;
+
+  weatherCont.append(desc, img);
 }
+
+getWeather();
