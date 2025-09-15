3b7391aba91908deafbbcb431c17a50390fe3316
Max Mattiasson <max@mattiasson.eu>
2025-09-15 10:19:40 +0200

    väder

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const weatherCont = document.querySelector(".weather-cont");
+const KEY = "b2024be8c564ed764512c35c213e201c";
 
-async function getWeather() {
-  const res = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=62.39129&lon=17.3063&appid=b2024be8c564ed764512c35c213e201c&lang=sv&units=metric"
-  );
-  const data = await res.json();
-  const weatherDesc = data.weather[0].description;
-  const desc = document.createElement("p");
-  weatherDesc.append(desc);
+async function getWeatherData() {
+  const lat = 62.39129;
+  const lon = 17.3063;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}&units=metric&lang=sv`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.log(`${response.status}`);
+    }
 
-  const icon = data.weather[0].icon;
-  const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
-  const img = document.createElement("img");
-  img.src = iconUrl;
+    const res = await response.json();
 
-  weatherCont.append(desc, img);
+    const temp = Math.round(res.main.temp);
+    const desc = res.weather[0].description;
+    const icon = res.weather[0].icon;
+
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    const img = document.createElement("img");
+    img.classList.add = "weather-icon";
+    img.src = iconUrl;
+    img.alt = desc;
+
+    weatherCont.textContent = `Just nu: Sundsvall ${temp}°C, ${desc}`;
+    weatherCont.prepend(img);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
-
-getWeather();
+getWeatherData();
